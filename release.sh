88f8fdbb86411731a9db7292b87ef167c13d5f1b
Bhushan S Khajurkar <bhushan.sk@bhushansk-mac.local>
2015-09-07 19:47:17 +0530

    Deleted the unncessary files

--- release.sh
+++ release.sh
@@ -6,6 +6,6 @@ cat pom.xml | grep -F '<version>' | head -1 | sed -e 's/<[^/]*>//g' -e 's/<.*>//
 set -e
 mvn clean install $*
 #mvn deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local $*
-mvn deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local $*
+mvn -X deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local $*
 #mvn deploy -q -DperformRelease=true -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-snapshots-local
 
